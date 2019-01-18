13c80e6ae6f66b67b079a9dffe7a32b742051a5c
zenjabba <zenjabba@users.noreply.github.com>
2019-01-17 19:13:18 -0500

    Update 40-postgres.sh
    https://github.com/oznu/docker-guacamole/issues/11
    
    Fixes this issue

--- root/etc/cont-init.d/40-postgres.sh
+++ root/etc/cont-init.d/40-postgres.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv sh
 
 mkdir -p /config/postgres
+mkdir -p /var/run/postgresql 
+chown postgres:postgres /var/run/postgresql
 chown -R postgres:postgres /config/postgres
 chmod 0700 /config/postgres
 
